cd9e39c7ced7f3876b1c784c4a855d8ff1b8533b
eksc <ek_baby@mail.ru>
2021-03-04 16:48:35 +0500

    Add project files.

---
+++ TestAppUsingTestClassLibraryReturnValue-NUPKG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAppUsingTestClassLibraryReturnValue_NUPKG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TestClassLibraryReturnValue.ReturnSumTwoValues.Sum(10, 20));
+        }
+    }
+}
